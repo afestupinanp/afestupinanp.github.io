d163b21992a24b373534c0eabee337188a5fa393
Andres Pelaez <andres.pelaez00@hotmail.com>
2025-10-24 21:15:42 -0500

    feat: added check how it was made link

--- app/components/PageFooter.vue
+++ app/components/PageFooter.vue
@@ -4,5 +4,8 @@
 <template>
     <footer class="py-10 w-full text-center">
         Andrés Felipe Estupiñan Peláez &copy; {{ new Date().getFullYear() }}
+        <a href="https://github.com/afestupinanp/afestupinanp.github.io" target="_blank" rel="noopener noreferrer">
+            {{ $t('footer-check-how') }}
+        </a>
     </footer>
 </template>
