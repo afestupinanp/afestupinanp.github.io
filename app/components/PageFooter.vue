9d5b94c50d2c20bcdf13bcf119330eabc9aa5760
Andres Pelaez <andres.pelaez00@hotmail.com>
2025-10-24 21:19:39 -0500

    fix: small design change

--- app/components/PageFooter.vue
+++ app/components/PageFooter.vue
@@ -3,9 +3,11 @@
 
 <template>
     <footer class="py-10 w-full text-center">
-        Andrés Felipe Estupiñan Peláez &copy; {{ new Date().getFullYear() }}
-        <a href="https://github.com/afestupinanp/afestupinanp.github.io" target="_blank" rel="noopener noreferrer">
-            {{ $t('footer-check-how') }}
-        </a>
+        Andrés Felipe Estupiñan Peláez &copy; {{ new Date().getFullYear() }}<br/>
+        <small>
+            <a href="https://github.com/afestupinanp/afestupinanp.github.io" target="_blank" rel="noopener noreferrer">
+                {{ $t('footer-check-how') }}
+            </a>
+        </small>
     </footer>
 </template>
